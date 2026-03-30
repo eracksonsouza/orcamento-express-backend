455d37a5e111c286967ece9f34de8fa33078406b
Erackson souza <souza.erackson@gmail.com>
2026-03-30 19:29:45 -0300

    feat: add quote routes with controllers and use cases for quote management

--- src/infra/http/routes/customer-routes.ts
+++ src/infra/http/routes/customer-routes.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from "fastify";
-import { CreateCustomerController } from "../controllers/create-customer-controller";
-import { GetCustomerController } from "../controllers/get-customer-controller";
-import { ListCustomersController } from "../controllers/list-customers-controller";
+import { CreateCustomerController } from "../controllers/customer/create-customer-controller";
+import { GetCustomerController } from "../controllers/customer/get-customer-controller";
+import { ListCustomersController } from "../controllers/customer/list-customers-controller";
 import { makeCreateCustomerUseCase } from "../factories/make-create-customer-use-case";
 import { makeGetCustomerUseCase } from "../factories/make-get-customer-use-case";
 import { makeListCustomersUseCase } from "../factories/make-list-customers-use-case";
